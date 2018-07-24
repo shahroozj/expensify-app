4cfb25e033cea666b8c7e420bcaa4110af8afebd
Shahrooz Jafari <scorpion_s@yahoo.com>
2018-07-24 15:47:31 +0800

    Connect to Redux

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
+import {addExpense} from './actions/expenses';
+import {setTextFilter} from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
-console.log(store.getState());
+
+const expenseOne = store.dispatch(addExpense({description : 'Water bill', amount: 100, createdAt: 0}));
+const expenseTwo = store.dispatch(addExpense({description : 'Gas bill', amount: 150, createdAt: 0}));
+
+//store.dispatch(setTextFilter('bill'));
+store.dispatch(setTextFilter('bill'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+
+console.log(visibleExpenses);
 
 ReactDOM.render(<AppRouter/>, document.getElementById('app'));
