9a4ccf33d888eaea48a4efe6b831bb87d4cf7ab5
Shahrooz Jafari <shahroozj@gmail.com>
2018-08-15 20:30:49 +0800

    Connect Redux to React

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import ExpenseList from './ExpenseList';
 
 const ExpenseDashboardPage = () =>(
     <div>
       This is dashboard of the application
+
+      <ExpenseList />
     </div>
 )
 
