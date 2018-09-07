106a836b778ca8a933d82c85a6f0a65e24f952b8
Shahrooz Jafari <scorpion_s@yahoo.com>
2018-09-07 15:38:57 +0800

    DateRange

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import moment from 'moment';
+import {SingleDatePicker} from 'react-dates';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+
+const now = moment();
+console.log(now.format('MMM Do, YYYY'));
 
 export default class ExpenseFrom extends React.Component{
-    state = {
-        description : '',
-        note : '',
-        amount : ''
+    constructor (props) {
+        super(props);
+        
+        this.state = {
+            description : props.expense ? props.expense.description : '',
+            note : props.expense ? props.expense.note : '',
+            amount : props.expense ? (props.expense.amount / 100).toString() : '',
+            createdAt : props.expense ? moment(props.expense.createdAt) : moment(),
+            calendarFocuced : false,
+            error : ''
+        }
     }
-
+    
     onDescriptionChange = (e) => {
         const description = e.target.value;
         this.setState(() => ({description}));
@@ -19,15 +33,42 @@ export default class ExpenseFrom extends React.Component{
 
     onAmountChange = (e) => {
         const amount = e.target.value;
-        if(amount.match(/^\d*(\.\d{0,2})?$/)){
+        if(!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)){
             this.setState(() => ({amount}));
+        }
+    }
+    onDateChange = (createdAt) =>{
+        if (createdAt){
+            this.setState(() => ({createdAt}));
+        }
+    }
+
+    onFocusChange = ({ focused }) => {
+        this.setState(() => ({calendarFocuced: focused}))
+    }
+
+    onSubmit = (e) =>{
+        e.preventDefault();
+
+        if (this.state.description == '' || this.state.amount ==''){
+            this.setState(() => ({error: 'Please fill desctiprion and amount'}));
+        }
+        else{
+            this.setState(() => ({error : ''}));
+            this.props.onSubmit({
+                description: this.state.description,
+                amount: parseFloat(this.state.amount, 10) * 100,
+                createdAt: this.state.createdAt.valueOf(),
+                note: this.state.note
+            });
         }
     }
 
     render(){
         return(
             <div>
-                <form>
+                {this.state.error && <p>{this.state.error}</p>}
+                <form onSubmit = {this.onSubmit}>
                     <input 
                     type = "text"
                     placeholder = "Description"
@@ -43,6 +84,14 @@ export default class ExpenseFrom extends React.Component{
                     onChange = {this.onAmountChange}
                     >
                     </input>
+                    <SingleDatePicker
+                    date = {this.state.createdAt}
+                    onDateChange = {this.onDateChange}
+                    focused =  {this.state.calendarFocuced}
+                    onFocusChange ={this.onFocusChange}
+                    numberOfMonths = {1}
+                    isOutsideRange = { () =>false }
+                    />
                     <textarea
                     placeholder="Add note for this expense (optional)"
                     value = {this.state.note}
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {removeExpense} from '../actions/expenses';
+import {Link} from 'react-router-dom';
 
-const ExpenseListItem = ({dispatch, id, description, amount, createdAt}) => (
+const ExpenseListItem = ({id, description, amount, createdAt, note}) => (
     <div>
-        <h3>{description}</h3>
+        <Link to={`/edit/${id}`}><h3>{description}</h3></Link>
         <p>{amount} - {createdAt}</p>
-        <button onClick={() => {
-            dispatch(removeExpense( { id } ));
-        }}>Remove</button>
+        <p>{note}</p>
     </div>
 )
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
