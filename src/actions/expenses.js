f03ec42609eb4176425ac13c13796d5b6adac06b
Shahrooz Jafari <scorpion_s@yahoo.com>
2019-01-17 09:59:24 +0800

    Setup startRemoveExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -47,6 +47,16 @@ export const removeExpense = ({id} = {}) => ({
     id 
 });
 
+export const startRemoveExpense = ({id} = {}) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).remove().then(()=>{
+            dispatch(removeExpense({
+                id
+            }));
+         });
+    };
+};
+
 //Edit Expense
 export const editExpense = (id, updates) =>({
     type : 'EDIT_EXPENSE',
